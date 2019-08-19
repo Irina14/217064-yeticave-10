60b2fcc04fbd4d1c4106e16a24360804b81bceab
Irina14 <ira1786@bk.ru>
2019-08-19 19:20:01 +0600

    Добавлены SQL-запросы

--- schema.sql
+++ schema.sql
@@ -21,7 +21,8 @@ CREATE TABLE lots (
   cost_start DECIMAL(8,2) NOT NULL,
   date_end DATETIME NOT NULL,
   step_rate INT NOT NULL,
-  winner_id INT NOT NULL
+  cost_current DECIMAL(8,2) NOT NULL,
+  winner_id INT DEFAULT 0;
 );
 
 CREATE TABLE rates (
